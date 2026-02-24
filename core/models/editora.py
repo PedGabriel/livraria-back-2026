12ffebaa8ff658f69b5bf3dba2d5394c0cea2b05
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2026-02-24 16:47:33 -0300

    FEAT: Criação da model de Categoria

---
+++ core/models/editora.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
